44607a1743b5f1a7160fc18d1125e636c8f5c4d0
biswesh456 <biswesh.mohapatra@iiitb.org>
2018-08-11 21:40:51 +0530

    changes to Dreg.sol

--- contracts/Dreg.sol
+++ contracts/Dreg.sol
@@ -4,8 +4,9 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Dreg is ownable {
   mapping(bytes32 => name) registry;
-  mapping(address => bool) users;
-  unit charge = 0.0001 ether;
+  mapping(address => uint) userMapping;
+  uint charge = 0.0001 ether;
+  address[] userList;
 
   modifier payEther() {
     require(msg.value == charge, "Minimum ether was not paid.");
@@ -13,14 +14,23 @@ contract Dreg is ownable {
   }
 
   function insert(string _num, string _name) external {
-    if (registry[keccak256(_num)].isValue) {
+    bytes32 hashedValue = keccak256(_num);
+    if (registry[hashedValue].isValue) {
       string _tempName = registry[keccak256(_num)];
       // needs to change
       if (_tempName.length < _name.length) {
-        registry[keccak256(_num)] = _name;
+        registry[hashedValue] = _name;
       }
     } else {
-      registry[keccak256(_num)] = _name;
+      registry[hashedValue] = _name;
+    }
+
+    // check the number of phone numbers that the user has created
+    if (!userMapping[msg.sender].isValue){
+      userMapping[msg.sender]  = 1;
+      userList.push(msg.sender);
+    } else {
+      userMapping[msg.sender] = userMapping[msg.sender] + 1;
     }
   }
 
@@ -37,7 +47,16 @@ contract Dreg is ownable {
     return charge;
   }
 
-  function distributeMoney() external onlyOwner{
+  function getBalance() external view onlyOwner return(uint) {
+    return this.balance;
+  }
 
+  // give the owner the cost of calling distributeMoney, distribute the rest amongst the others
+  function distributeMoney() external onlyOwner{
+    owner.transfer(this.balance * 0.1);
+    uint distributedValue = (this.balance / userList.length);
+    for (uint i=0; i<userList.length; i++) {
+      userList[i].transfer(distributedValue);
+    }
   }
 }
