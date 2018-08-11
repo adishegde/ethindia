c2102480bbc91b4ad67753c80aaf6702e7622ff5
Satvik Ramaprasad <satviksr@gmail.com>
2018-08-11 23:19:17 +0530

    Specs

--- contracts/Dreg.sol
+++ contracts/Dreg.sol
@@ -1,42 +1,38 @@
 pragma solidity ^0.4.21;
 
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/ownership/Ownable.sol";
 
-contract Dreg is ownable {
-  mapping(bytes32 => name) registry;
+contract Dreg is Ownable {
+  mapping(bytes32 => string) registry;
   mapping(address => uint) userMapping;
   uint charge = 0.0001 ether;
   address[] userList;
 
+  event sendName(string name);
+
   modifier payEther() {
-    require(msg.value == charge, "Minimum ether was not paid.");
+    require(msg.value >= charge, "Minimum ether was not paid.");
     _;
   }
 
   function insert(string _num, string _name) external {
-    bytes32 hashedValue = keccak256(_num);
-    if (registry[hashedValue].isValue) {
-      string _tempName = registry[keccak256(_num)];
-      // needs to change
-      if (_tempName.length < _name.length) {
-        registry[hashedValue] = _name;
-      }
-    } else {
-      registry[hashedValue] = _name;
-    }
+    bytes32 hashedValue = keccak256(abi.encodePacked(_num));
+
+	  string memory _tempName = registry[hashedValue];      // needs to change
+	  if (bytes(_tempName).length < bytes(_name).length) {
+	    registry[hashedValue] = _name;
+	  }
 
     // check the number of phone numbers that the user has created
-    if (!userMapping[msg.sender].isValue){
-      userMapping[msg.sender]  = 1;
+    if (userMapping[msg.sender]==0){
       userList.push(msg.sender);
-    } else {
-      userMapping[msg.sender] = userMapping[msg.sender] + 1;
     }
+	userMapping[msg.sender] = userMapping[msg.sender] + 1;
   }
 
-  function getName(string _num) external view payable payEther returns(string) {
-    require(registry[keccak256(_num)].isValue, "Number not registered");
-    return registry[keccak(_num)];
+  function getName(string _num) external payable payEther  {
+    /* require(bytes(registry[keccak256(abi.encodePacked(_num))]).length>0, "Number not registered"); */
+    sendName(registry[keccak256(abi.encodePacked(_num))]);
   }
 
   function setCharge(uint _charge) external onlyOwner {
@@ -47,14 +43,14 @@ contract Dreg is ownable {
     return charge;
   }
 
-  function getBalance() external view onlyOwner return(uint) {
-    return this.balance;
+  function getBalance() external view onlyOwner returns(uint) {
+    return address(this).balance;
   }
 
   // give the owner the cost of calling distributeMoney, distribute the rest amongst the others
   function distributeMoney() external onlyOwner{
-    owner.transfer(this.balance * 0.1);
-    uint distributedValue = (this.balance / userList.length);
+    owner.transfer(address(this).balance / 10);
+    uint distributedValue = (address(this).balance / userList.length);
     for (uint i=0; i<userList.length; i++) {
       userList[i].transfer(distributedValue);
     }
--- test/dreg_spec.js
+++ test/dreg_spec.js
@@ -2,26 +2,38 @@ const Dreg = require("Embark/contracts/Dreg");
 
 let accounts;
 
-config({
-    contracts: {
-	Dreg: {
-	}
+config(
+    {
+        contracts: {
+            Dreg: {}
+        }
+    },
+    (err, allAccnts) => {
+        accounts = allAccnts;
     }
-}, (err, allAccnts) => {
-    accounts = allAccnts;
-});
+);
 
 const phoneNumber = "12345678998";
 const name = "ABC";
 
 contract("Dreg contract", () => {
     it("successfully adds a mapping from phone number to user", async () => {
-	const insertTx = await Dreg.methods.insert(phoneNumber, name).send();
-	assert.equal(insertTx, true);
+        const insertTx = await Dreg.methods.insert(phoneNumber, name).send();
+        assert.equal(insertTx.status, true);
     });
 
     it("retrieves name from phone number", async () => {
-	const retName = await Dreg.methods.getName(phoneNumber).send();
-	assert.equal(name, retName);
+        await Dreg.methods
+            .getName(phoneNumber)
+            .send({ value: "100000000000000" })
+            .on("sendName", function(name) {
+                assert.equal(name, retName);
+            });
+    });
+
+    it("Balance", async () => {
+        const balance = await Dreg.methods.getBalance().call();
+        console.log(balance);
+        assert.equal(100000000000000, balance);
     });
 });
