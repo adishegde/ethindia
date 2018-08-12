2e0ba02f4fc64252f1765c554decdd088bdcebba
biswesh456 <biswesh.mohapatra@iiitb.org>
2018-08-12 15:43:03 +0530

    comments added to Dreg.sol

--- contracts/Dreg.sol
+++ contracts/Dreg.sol
@@ -3,26 +3,40 @@ pragma solidity ^0.4.21;
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/ownership/Ownable.sol";
 
 contract Dreg is Ownable {
+  // mapping between the hash of a number and the corresponding name
   mapping(bytes32 => string) registry;
+  // mapping between user and their number of contributions
   mapping(address => uint) userMapping;
+  // mapping to check the last month a user had last taken his/her reward
   mapping(address => uint) monthlyWithdrawal;
-  //uint charge = 0.0001 ether; // Charge for each query.
-  uint charge = 0.1 ether; // Charge for each query.
+  // Charge for each query.
+  uint charge = 0.1 ether;
 
-  // uint period = 1 * 86400; // One month time period
+  // One month time period
+  /* uint period = 28 * 86400; */
   uint period = 1; // For development we use a time period of 1 second
+
+  // stores the previous time to keep a count of the month
   uint previousMonthTime = now;
+  // stores the current month
   uint latestMonthNumber = 1;
+  // amount to be distributed in a particular period to all the users
   uint distributedAmount;
+  // list of all the contributers
   address[] userList;
 
   event sendName(string name);
 
+  // Ensures that a user who wants to query a number pays a minimum charge
   modifier payEther() {
     require(msg.value >= charge, "Minimum ether was not paid.");
     _;
   }
 
+  /*  The modifier checks if the function has been called after a given period,
+      if yes then reset the previousMonthTime to the new value, increase the latestMonthNumber(month counter)
+      and calculate the distributed amout for each user for the next period(usually 28 days)
+      */
   modifier changeMonth() {
     if (now > previousMonthTime + period) {
       previousMonthTime = previousMonthTime + ((now - previousMonthTime)/period) * period;
@@ -32,6 +46,11 @@ contract Dreg is Ownable {
     _;
   }
 
+   /* The function is called when a user wants to  insert a new data to the smart contracts
+      The number is hashed and mapped to the name provided by the user.
+      If the number is already present in the map then we decide to replace it on the basis of the length of the string
+      We also insert the new user to the contributers map
+   */
   function insert(string _num, string _name) external {
     bytes32 hashedValue = keccak256(abi.encodePacked(_num));
 
@@ -47,12 +66,12 @@ contract Dreg is Ownable {
 	userMapping[msg.sender] = userMapping[msg.sender] + 1;
   }
 
+  // the following function gets called when a user does a query for a number
   function getName(string _num) external payable payEther  {
-    /* require(bytes(registry[keccak256(abi.encodePacked(_num))]).length>0, "Number not registered"); */
-    /* emit sendName("ABC"); */
     emit sendName(registry[keccak256(abi.encodePacked(_num))]);
   }
 
+  // There exists an owner who sets the charge for the getName function
   function setCharge(uint _charge) external onlyOwner {
     charge = _charge;
   }
@@ -65,7 +84,10 @@ contract Dreg is Ownable {
     return address(this).balance;
   }
 
-  // give the owner the cost of calling distributeMoney, distribute the rest amongst the others
+   /* The following function gives rewards to the users who are contributing to the data pool of the smart contract.
+     The first require statement ensures that only those users who have contributed can get the reward
+     The second require statement ensures that a contributer can get the reward only once in a given period(28 days here)
+   */
   function getMoney() external payable changeMonth() {
     require(userMapping[msg.sender] != 0, "To get rewards kindly contribute to DReg");
     require(monthlyWithdrawal[msg.sender] != latestMonthNumber, "Already withdrew the reward for current month");
